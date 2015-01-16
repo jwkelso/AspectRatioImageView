588a4053c3cfa51f30227ea1fe8f1a138bf73313
James Kelso <james.w.kelso@gmail.com>
2015-01-16 18:00:34 -0500

    Add license to widget.

--- library/src/main/java/com/jameskelso/android/widget/DefaultSizeImageView.java
+++ library/src/main/java/com/jameskelso/android/widget/DefaultSizeImageView.java
@@ -1,4 +1,28 @@
-package com.jameskelso.android.widget;
+/*
+* The MIT License (MIT)
+
+* Copyright (c) 2015 James W Kelso
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* */
+
+ package com.jameskelso.android.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
@@ -29,6 +53,7 @@ import android.widget.ImageView;
  * calculate an aspect ratio and update the measured dimension of the view to match what it will be
  * when the image load is complete.
  */
+
 public class DefaultSizeImageView extends ImageView {
     // Aspect ratio is width / height
     protected float mAspectRatio;
